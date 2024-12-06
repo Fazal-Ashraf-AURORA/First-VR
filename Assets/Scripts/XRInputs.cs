879c34f0a5267794cf4c14acd292a1926607204b
AURORA <fazalashrafofficial@gmail.com>
2024-12-06 12:47:39 +0530

    Added XRInputBase Script

--- Assets/Scripts/XRInputs.cs
+++ Assets/Scripts/XRInputs.cs
@@ -8,19 +8,65 @@ public class XRInputs : MonoBehaviour
     [Header("Right Controller")]
     [SerializeField] private InputActionReference _rightHandTrigger;
     [SerializeField] private InputActionReference _rightHandGrab;
+    [SerializeField] private InputActionReference _rightHandPrimaryButton;
+    [SerializeField] private InputActionReference _rightHandSecondaryButton;
 
     [Header("Left Controller")]
     [SerializeField] private InputActionReference _leftHandTrigger;
     [SerializeField] private InputActionReference _leftHandGrab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private InputActionReference _leftHandPrimaryButton;
+    [SerializeField] private InputActionReference _leftHandSecondaryButton;
 
     // Update is called once per frame
     void Update()
     {
-        
+        RightHandController();
+
+        LeftHandController();
+    }
+    private void RightHandController()
+    {
+       if(_rightHandTrigger.action.ReadValue<float>() > 0.5f)
+       {
+       Debug.Log("Right Hand Trigger Button Pressed");
+       }
+
+       if(_rightHandGrab.action.ReadValue<float>() > 0.5f)
+       {
+       Debug.Log("Right Hand Grab Button Pressed");
+       }
+
+       if(_rightHandPrimaryButton.action.ReadValue<float>() > 0.5f)
+       {
+       Debug.Log("Right Hand Primary Button Pressed");
+       }
+
+       if(_rightHandSecondaryButton.action.ReadValue<float>() > 0.5f)
+       {
+       Debug.Log("Right Hand Secondary Button Pressed");
+       }
+    }
+
+    private void LeftHandController()
+    {
+        if (_leftHandTrigger.action.ReadValue<float>() > 0.5f)
+        {
+            Debug.Log("Left Hand Trigger Button Pressed");
+        }
+
+        if (_leftHandGrab.action.ReadValue<float>() > 0.5f)
+        {
+            Debug.Log("Left Hand Grab Button Pressed");
+        }
+
+        if (_leftHandPrimaryButton.action.ReadValue<float>() > 0.5f)
+        {
+            Debug.Log("Left Hand Primary Button Pressed");
+        }
+
+        if (_leftHandSecondaryButton.action.ReadValue<float>() > 0.5f)
+        {
+            Debug.Log("Left Hand Secondary Button Pressed");
+        }
     }
 }
